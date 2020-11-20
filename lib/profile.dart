69195d3cb74c10dccbe78ceb8c41c7768d5d6b4a
zuhanl2 <zuhanl2@illinois.edu>
2020-11-19 20:55:12 -0600

    profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,21 +6,47 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Profile Page!"),
-            Text("Profile email: " + FirebaseAuth.instance.currentUser.email),
-            Text("Profile uid: " + FirebaseAuth.instance.currentUser.uid),
-            RaisedButton(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Profile Page!",
+              style: TextStyle(
+                  fontSize: 50.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -3.0)),
+          ElevatedButton(
+              clipBehavior: Clip.antiAlias,
+              onPressed: () => {},
+              style: ButtonStyle(
+                elevation: MaterialStateProperty.all(10.0),
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                shape: MaterialStateProperty.all(CircleBorder()),
+                padding: MaterialStateProperty.all(EdgeInsets.zero),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                enableFeedback: true,
+              ),
+              child: Image(
+                // Doesn't work correctly; needs to redraw/set state on auth state change
+                image: AssetImage("assets/person.png"),
+                fit: BoxFit.cover,
+                width: 200.0,
+                height: 200.0,
+              )),
+          Text("Profile email: " + FirebaseAuth.instance.currentUser.email,
+              style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: -0.5)),
+          // Text("Profile uid: " + FirebaseAuth.instance.currentUser.uid),
+          RaisedButton(
               onPressed: () => FirebaseAuth.instance.signOut(),
               color: Colors.red,
-              child: Text("Sign out")
-            )
-          ],
-        )
-      )
-    );
+              child: Text("Sign out"))
+        ],
+      ),
+    ));
   }
 }
