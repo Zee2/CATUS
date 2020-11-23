82e854206d306101960e1fdae201a6f08e8cb14b
Keval Morabia <kevalmorabia97@gmail.com>
2020-11-22 23:15:22 -0600

    QuickTeam formation results screen added

--- lib/quickTeamFormation.dart
+++ lib/quickTeamFormation.dart
@@ -6,7 +6,6 @@ class QuickTeamFormation extends StatefulWidget {
 }
 
 class _QuickTeamFormationState extends State<QuickTeamFormation> {
-
   final _formKey = GlobalKey<FormState>(); // Key for validating form
 
   String eventName = "";
@@ -30,6 +29,18 @@ class _QuickTeamFormationState extends State<QuickTeamFormation> {
         appBar: AppBar(
           title: Text('Quick Team Formation'),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if(names.length > 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => QuickTeamResults(names, teamSize)),
+              );
+            }
+          },
+          child: Icon(Icons.shuffle),
+        ),
         body: Form(
             key: _formKey,
             child: Padding(
@@ -50,7 +61,7 @@ class _QuickTeamFormationState extends State<QuickTeamFormation> {
                     decoration: InputDecoration(
                       labelText: "Team Size",
                     ),
-                    onChanged: (value) => {teamSize = value as int},
+                    onChanged: (value) => {teamSize = int.parse(value)},
                     validator: (value) {
                       if (value.isEmpty)
                         return "Please enter your team size";
@@ -86,5 +97,73 @@ class _QuickTeamFormationState extends State<QuickTeamFormation> {
                         }))
               ]),
             )));
+  }
+}
+
+class QuickTeamResults extends StatelessWidget {
+  List<String> names = [];
+  int teamSize;
+
+  QuickTeamResults(List<String> names, int teamSize) {
+    for (int i = 0; i < names.length; i++) {
+      this.names.add(names[i]);
+    }
+    this.names.shuffle();
+
+    this.teamSize = teamSize;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Team Results'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => QuickTeamResults(names, teamSize)),
+          );
+        },
+        child: Icon(Icons.shuffle),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: names.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Column members = Column(
+                      children: [],
+                    );
+
+                    if (index % teamSize == 0) {
+                      members.children.add(Container(
+                        height: 20,
+                        margin: EdgeInsets.all(2),
+                        color: Colors.white,
+                        child: Text("Team ${index ~/ teamSize + 1}"),
+                      ));
+                    }
+
+                    members.children.add(Container(
+                      height: 50,
+                      margin: EdgeInsets.all(2),
+                      color: Colors.blue[400],
+                      child: Center(
+                          child: Text(
+                        '${names[index]}',
+                        style: TextStyle(fontSize: 18),
+                      )),
+                    ));
+
+                    return members;
+                  }))
+        ],
+      ),
+    );
   }
 }
