c3a845755e331f6e30b0febb11cd4d53d3ad44ac
Keval Morabia <kevalmorabia97@gmail.com>
2020-11-25 23:35:13 -0600

    Groups screen (minimal)

--- lib/groups.dart
+++ lib/groups.dart
@@ -1,8 +1,8 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:catus/header.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Tags extends StatelessWidget {
   const Tags(this.tags);
@@ -11,18 +11,14 @@ class Tags extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: List.generate(tags.length, (index) {
-        return Container(
-          padding: EdgeInsets.only(right: 5),
-          child: GroupTag(tags[index])
-        );  
-      })
-    );
+        children: List.generate(tags.length, (index) {
+      return Container(
+          padding: EdgeInsets.only(right: 5), child: GroupTag(tags[index]));
+    }));
   }
 }
 
-class GroupTag extends StatelessWidget{
-
+class GroupTag extends StatelessWidget {
   const GroupTag(this.text);
 
   final String text;
@@ -31,15 +27,151 @@ class GroupTag extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100.0),
-        //border: Border.all(color: Colors.white, width: 2.0),
-        color: Colors.black.withOpacity(0.3)
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(left:11.0, right: 11.0, top: 5.0, bottom: 4.0),
-        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 15),),
-      )
-    );
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100.0),
+            //border: Border.all(color: Colors.white, width: 2.0),
+            color: Colors.black.withOpacity(0.3)),
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: 11.0, right: 11.0, top: 5.0, bottom: 4.0),
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 15),
+          ),
+        ));
+  }
+}
+
+class GroupScreen extends StatefulWidget {
+  @override
+  _GroupScreenState createState() => _GroupScreenState();
+}
+
+class _GroupScreenState extends State<GroupScreen> {
+  Stream<QuerySnapshot> userGroups;
+
+  @override
+  void initState() {
+    super.initState();
+
+    var user = FirebaseAuth.instance.currentUser;
+    userGroups = FirebaseFirestore.instance
+        .collection("groups")
+        .where('users', arrayContains: user.uid)
+        .snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: userGroups,
+        builder: (context, snapshot) {
+          print("Streambuilder Test!");
+
+          if (snapshot.hasError) {
+            print("Snapshot error: " + snapshot.error.toString());
+            return Text("Something wrong!");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("Snapshot waiting");
+            return Text("Something wrong!");
+          }
+
+          print("Found " + snapshot.data.docs.length.toString() + "documents");
+
+          List<Widget> listElements = [];
+          listElements.add(SafeArea(
+              child: Header(
+            text: "Groups",
+            showProfile: false,
+            showText: true,
+          )));
+
+          for (DocumentSnapshot doc in snapshot.data.docs) {
+            listElements.add(Text(
+              doc['name'],
+              style: Theme.of(context).textTheme.headline4,
+            ));
+          }
+
+          listElements.add(ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddGroups()),
+              );
+            },
+            child: Text("Add Groups"),
+          ));
+
+          return Scaffold(
+            resizeToAvoidBottomPadding: false,
+            body: Column(
+              children: listElements,
+            ),
+          );
+        });
+  }
+}
+
+class AddGroups extends StatefulWidget {
+  @override
+  _AddGroupsState createState() => _AddGroupsState();
+}
+
+class _AddGroupsState extends State<AddGroups> {
+  Stream<QuerySnapshot> allGroups;
+
+  @override
+  void initState() {
+    super.initState();
+
+    allGroups = FirebaseFirestore.instance.collection("groups").snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: allGroups,
+        builder: (context, snapshot) {
+          print("Streambuilder Test!");
+
+          if (snapshot.hasError) {
+            print("Snapshot error: " + snapshot.error.toString());
+            return Text("Something wrong!");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("Snapshot waiting");
+            return Text("Something wrong!");
+          }
+
+          print("Found " + snapshot.data.docs.length.toString() + "documents");
+
+          List<Widget> listElements = [];
+          listElements.add(SafeArea(
+              child: Header(
+            text: "Add Groups",
+            showProfile: false,
+            showText: true,
+          )));
+
+          for (DocumentSnapshot doc in snapshot.data.docs) {
+            listElements.add(ElevatedButton(
+              child: Text(doc['name']),
+              onPressed: () {
+                // Ideally store this user's uid to the group
+              },
+            ));
+          }
+
+          return Scaffold(
+            resizeToAvoidBottomPadding: false,
+            body: Column(
+              children: listElements,
+            ),
+          );
+        });
   }
 }
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:catus/groups.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -7,86 +8,108 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          color: Colors.white,
-          child: Center(
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 500.0),
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(  
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  boxShadow: [BoxShadow(color:Colors.black.withOpacity(0.1), spreadRadius: 0, blurRadius: 20, offset: Offset(0,3))]
-                ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("John Doe", style: Theme.of(context).textTheme.headline3,),
-                          Text(FirebaseAuth.instance.currentUser.email),
-                          Text("Email verified: " + FirebaseAuth.instance.currentUser.emailVerified.toString()),
-                        ],
-                      ),
-                      Hero(
-                        tag: "profileButton",
-                        child: ElevatedButton(
-                          clipBehavior: Clip.antiAlias,
-                          onPressed: () => {},
-                          style: ButtonStyle(
-                            elevation: MaterialStateProperty.all(10.0),
-                            backgroundColor: MaterialStateProperty.all(Colors.white),
-                            shape: MaterialStateProperty.all(CircleBorder()),
-                            padding: MaterialStateProperty.all(EdgeInsets.zero),
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            enableFeedback: true,
-                          ),
-                          child: StreamBuilder<User>(
-                            stream: FirebaseAuth.instance.authStateChanges(),
-                            builder: (context, value){
-                              return Image(
-                                image: value == null ? AssetImage("assets/anonUser.png") : AssetImage("assets/person.png"),
-                              fit: BoxFit.cover,
-                              width: 120.0,
-                              height: 120.0,
-                              );
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 500.0),
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 0,
+                          blurRadius: 20,
+                          offset: Offset(0, 3))
+                    ]),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "John Doe",
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            Text(FirebaseAuth.instance.currentUser.email),
+                            Text("Email verified: " +
+                                FirebaseAuth.instance.currentUser.emailVerified
+                                    .toString()),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => GroupScreen()),
+                                );
+                              },
+                              child: Text("Groups"),
+                            )
+                          ],
+                        ),
+                        Hero(
+                            tag: "profileButton",
+                            child: ElevatedButton(
+                                clipBehavior: Clip.antiAlias,
+                                onPressed: () => {},
+                                style: ButtonStyle(
+                                  elevation: MaterialStateProperty.all(10.0),
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  shape:
+                                      MaterialStateProperty.all(CircleBorder()),
+                                  padding: MaterialStateProperty.all(
+                                      EdgeInsets.zero),
+                                  tapTargetSize:
+                                      MaterialTapTargetSize.shrinkWrap,
+                                  enableFeedback: true,
+                                ),
+                                child: StreamBuilder<User>(
+                                  stream:
+                                      FirebaseAuth.instance.authStateChanges(),
+                                  builder: (context, value) {
+                                    return Image(
+                                      image: value == null
+                                          ? AssetImage("assets/anonUser.png")
+                                          : AssetImage("assets/person.png"),
+                                      fit: BoxFit.cover,
+                                      width: 120.0,
+                                      height: 120.0,
+                                    );
+                                  },
+                                  // Doesn't work correctly; needs to redraw/set state on auth state change
+                                ))),
+                      ],
+                    ),
+                    //Container(height: 40,),
+                    // Text("Profile uid: " + FirebaseAuth.instance.currentUser.uid),
+                    Row(
+                      children: [
+                        FloatingActionButton.extended(
+                            onPressed: () {
+                              FirebaseAuth.instance.signOut();
+                              Navigator.of(context).pop();
                             },
-                            // Doesn't work correctly; needs to redraw/set state on auth state change
-                            
-                          )
-                        )
-                      ),
-                    ],
-                  ),
-                  //Container(height: 40,),
-                  // Text("Profile uid: " + FirebaseAuth.instance.currentUser.uid),
-                  Row(children: [
-                    FloatingActionButton.extended(
-                      onPressed: () {
-                        FirebaseAuth.instance.signOut();
-                        Navigator.of(context).pop();
-                      },
-                      heroTag: null,
-                      backgroundColor: Colors.redAccent,
-                      label: Text("SIGN OUT"),
-                      icon: Icon(Icons.close)
+                            heroTag: null,
+                            backgroundColor: Colors.redAccent,
+                            label: Text("SIGN OUT"),
+                            icon: Icon(Icons.close))
+                      ],
                     )
-                  ],)
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        )
-         
-      );
+            )));
   }
 }
